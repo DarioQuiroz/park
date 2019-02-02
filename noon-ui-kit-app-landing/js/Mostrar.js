1000931e3a024f98a06bae6ac851bfae3e85c123
dario <gil.q.c.dq@gmail.com>
2019-02-02 13:02:45 -0600

    add componentes

---
+++ noon-ui-kit-app-landing/js/Mostrar.js
@@ -0,0 +1,20 @@
+
+function mostrarvalidar(){
+    document.getElementById("validar").style.display="block";
+    document.getElementById("mostrar").style.display="none";
+}
+function ocultarvalidar(){ 
+
+    document.getElementById("validar").style.display="none";
+    document.getElementById("mostrar").style.display="none";
+}
+ function mostrarformulario(){
+    document.getElementById("formulario").style.display="block";
+
+ }
+function ocultartodo(){
+    document.getElementById("validar").style.display="none";
+    document.getElementById("mostrar").style.display="block";
+    document.getElementById("formulario").style.display="none";
+
+}
