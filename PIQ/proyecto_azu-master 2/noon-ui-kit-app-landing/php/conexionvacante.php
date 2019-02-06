a8e660e956f998ea30c2bba4fe0aaf02f02b4069
Unknown <unknown@example.com>
2019-02-05 22:08:02 -0600

    add

---
+++ PIQ/proyecto_azu-master 2/noon-ui-kit-app-landing/php/conexionvacante.php
@@ -0,0 +1,12 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "vacantes");
+
+if (mysqli_connect_errno()) {
+die("No se puede conectar a la base de datos:" . mysqli_connect_error());
+}else{
+
+    echo "conexion exitosa";
+}
+
+
+?>
---
+++ PIQ/proyecto_azu-master 2/noon-ui-kit-app-landing/php/insertarvacante.php
@@ -0,0 +1,25 @@
+<?php 
+include('conexionvacante.php');
+if (isset($_POST['insertar'])) {
+$empresa=$_POST['emp_vac'];
+$puesto=$_POST['puesto'];
+$perfil=$_POST['Perfil_puesto'];
+$experiencia=$_POST['expe'];
+$idiomas=$_POST['idioma_add'];
+$nombre=$_POST['nom'];
+$telefono=$_POST['tel'];
+$correo=$_POST['correo'];
+
+
+$peticion="INSERT INTO tvacantes (emp_vac,puesto,Perfil_puesto,expe,idioma_add,nom,tel,correo) 
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $conn->prepare($peticion);
+$stmt->bind_param($empresa, $puesto, $perfil, $experiencia, $idiomas, $nombre, $telefono, $correo);
+$stmt->execute();
+$stmt->close();
+
+echo "Registro Insertado";
+$conn->close();
+}
+?>
