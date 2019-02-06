abd65249e338b34e8481420bf4e1dc0252d82970
Unknown <unknown@example.com>
2019-02-05 22:08:28 -0600

    replace

--- PIQ/proyecto_azu-master 2/noon-ui-kit-app-landing/js/Mostrar.js
+++ PIQ/proyecto_azu-master 2/noon-ui-kit-app-landing/js/Mostrar.js
@@ -1,8 +1,27 @@
 
+   var correo = null;
+   var contraseña = null;
+   
+
+       //submit
+       function recuperar(){
+           correo = document.getElementById("inputEmail").value;
+           contraseña = document.getElementById("inputPassword").value;
+           if (correo=="correodeprueba@prueba.com" && contraseña=="contraseña") {
+            alert(correo+" datos muy correctos "+contraseña);
+ 
+    document.getElementById("formulario").style.display="block";
+           } else {
+               alert("datos incorrectos");
+              
+           }
+       
+       };
+
 function mostrarvalidar(){
     document.getElementById("validar").style.display="block";
     document.getElementById("mostrar").style.display="none";
-}
+}/*
 function ocultarvalidar(){ 
 
     document.getElementById("validar").style.display="none";
@@ -18,3 +37,6 @@ function ocultartodo(){
     document.getElementById("formulario").style.display="none";
 
 }
+
+
+*/
